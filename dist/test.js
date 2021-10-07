0b3871bb03d2aa200fd7e8c6bc02cbf63a05d79d
Khunhour Morm <khonhourmorm@gmail.com>
2021-10-08 04:18:08 +0900

    add new icons work on form, and add remove projects, update number status a bit

--- dist/test.js
+++ dist/test.js
@@ -11,6 +11,13 @@ const showForm = () => {
     const projectForm = document.querySelector("#projectForm");
     projectForm.style.cssText = "visibility: visible; position:relative;";
 }
+// create a span icon of google material icons
+const createSpanIcon = (name) => {
+    const icon = document.createElement('span');
+    icon.classList.add("material-icons-round");
+    icon.textContent = name;
+    return icon;
+}
 // create a project and add it to the list of projects 
 const addProject = (textInput) => {
     const project = document.querySelector('.project');
@@ -18,25 +25,48 @@ const addProject = (textInput) => {
     const button = document.createElement('button');
     project.insertBefore(button, form);
 
-    const menuIcon = document.createElement('span');
-    menuIcon.classList.add("material-icons-round");
-    menuIcon.textContent = "menu";
-
+    const menuIcon = createSpanIcon("menu");
     const projectName = document.createTextNode(textInput);
+
     button.appendChild(menuIcon);
     button.appendChild(projectName);
+
+    const removediv = document.createElement('div');
+    removediv.classList.add('removeProject');
+    button.appendChild(removediv);
+
+    const removeIcon = createSpanIcon("close");
+    removediv.appendChild(removeIcon);
+
+    // activateEventListener(removediv);
+
+    removediv.addEventListener("click", removeProject);
 }
 
+//add event Listener to buttons
+// const activateEventListener = (div) => {
+//     div.addEventListener("click", )
+// }
+
+//hide form
 const hideForm = () =>{
     const projectForm = document.querySelector("#projectForm");
     const projectInput = document.querySelector('#projectInput');
-
 
     projectInput.value = "";
     projectForm.style.cssText = "visibility: hidden; position:absolute;";
     console.log(projectInput);
     
 }
+//remove project from list
+const removeProject = () =>{
+    //need data set to specific div
+    const remove = document.querySelector(".removeProject");
+    console.log("imma remove this");
+    console.log(remove);
+}
+
+
 
 // process inputed text, add project, and hide form again 
 const processProjectInput = (e) => {
@@ -46,8 +76,27 @@ const processProjectInput = (e) => {
     e.preventDefault();
 }
 
+const cancel = document.querySelector(".projectCancelBtn");
+cancel.addEventListener("click", hideForm);
+
 const add = document.getElementById('addListProject');
 add.addEventListener("click", showForm);
 
 const submit = document.getElementById("projectForm");
 submit.addEventListener("submit", processProjectInput);
+
+
+const check = document.querySelector(".unchecked");
+check.addEventListener("click", turnCheckMark);
+
+function turnCheckMark(){
+    if(check.classList.contains("checked")){
+        check.classList.remove("checked");
+    }
+    else{
+        check.classList.add("checked");
+    }
+}
+
+
+
